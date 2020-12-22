1b75fc19eccc4a9a5e5c637e6b3f5beeed050ce0
sivasai <57858904+sivasai9398@users.noreply.github.com>
2020-12-22 17:16:48 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask,render_template,request,url_for
+import pickle
+import sklearn
+import pandas as pd
+import joblib
+model = joblib.load(open('ensemble.pkl', 'rb'))
+app=Flask(__name__)
+@app.route("/home",methods=["GET","POST"])
+def home():
+    return render_template('dia.html')
+@app.route("/pr",methods=["GET","POST"])
+def sad():
+    if request.method == 'GET':
+        return render_template('dia.html')
+
+    if request.method == 'POST':
+        BP = request.form['BP']
+        BMI=request.form['BMI']
+        DPG=request.form['DPF']
+        preg=request.form['preg']
+        gluc=request.form['Glucose']
+        insulin=request.form['Insulin']
+        Age=request.form['Age']
+        #skin=request.form['skin']
+        X=pd.DataFrame([[preg,gluc,BP,insulin,BMI,DPG,Age]],dtype=object)
+        prediction=model.predict(X)
+        if prediction==0:
+            print("U don't have any chances of diabetes")
+            return render_template("dia2.html")
+        elif prediction==1:
+            print("U have a chance of diabetes")
+            return render_template("dia1.html")
+        else:
+            return render_template('dia.html')
+if __name__ == "__main__":
+    app.run(debug=True)
+#infile.close()
